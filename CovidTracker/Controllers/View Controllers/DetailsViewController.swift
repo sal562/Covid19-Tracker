2b54b744402bfb448119513e991aa6dca656e899
Sal <salmanamer@gmail.com>
2020-03-27 06:22:22 -0700

    detail view working - UI ugly AF.

--- CovidTracker/Controllers/View Controllers/DetailsViewController.swift
+++ CovidTracker/Controllers/View Controllers/DetailsViewController.swift
@@ -9,22 +9,59 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
+    
+    var entries: Entry?
+    
+    //IB Outlets
+    @IBOutlet weak var countryFlag: UIImageView!
+    @IBOutlet weak var countryNameLbl: UILabel!
+    @IBOutlet weak var lastUpdatedDateLbl: UILabel!
+    @IBOutlet weak var totalCasesLbl: UILabel!
+    @IBOutlet weak var totalCasesTodayLbl: UILabel!
+    @IBOutlet weak var totalDeathLbl: UILabel!
+    @IBOutlet weak var totalDeathsTodayLbl: UILabel!
+    @IBOutlet weak var totalActiveLbl: UILabel!
+    @IBOutlet weak var totalCriticalLbl: UILabel!
+    @IBOutlet weak var casesPer1MLbl: UILabel!
+    @IBOutlet weak var deathsPer1MLbl: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateViews()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        guard let entries = entries else { return }
+        countryNameLbl.text = entries.country
+        lastUpdatedDateLbl.text = "Last Updated: \(Date())"
+        totalCasesLbl.text = "\(entries.cases)"
+        totalCasesTodayLbl.text = "\(entries.todayCases)"
+        totalDeathLbl.text = "\(entries.deaths)"
+        totalDeathsTodayLbl.text = "\(entries.todayDeaths)"
+        totalActiveLbl.text = "\(entries.active)"
+        totalCriticalLbl.text = "\(entries.critical)"
+//        casesPer1MLbl.text = "\(entries)"
+        
+        // Get Flag image from URL
+        let countryFlagImg = entries.countryInfo.flag
+        let request = URLRequest(url: countryFlagImg)
+        
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            if let error = error {
+                print("Error fetching large image: \(error)")
+                return
+            }
+            guard let data = data else {
+                print("Error with flag image data: \(error)")
+                return
+            }
+            DispatchQueue.main.async {
+                self.countryFlag.image = UIImage(data: data)
+            }
+        }.resume()
+        
     }
-    */
-
+ 
 }
--- CovidTracker/Controllers/View Controllers/MainTableViewController.swift
+++ CovidTracker/Controllers/View Controllers/MainTableViewController.swift
@@ -130,14 +130,21 @@ class MainTableViewController: UITableViewController, UISearchBarDelegate {
     }
     */
 
-    /*
+ 
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "DetailsViewSegue" {
+            if let destinationVC = segue.destination as? DetailsViewController,
+                let indexPath = tableView.indexPathForSelectedRow {
+                let entry = apiController.entries[indexPath.row]
+                destinationVC.entries = entry
+                 
+            }
+            
+        }
     }
-    */
+ 
 
 }
